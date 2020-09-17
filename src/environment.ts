6c4db0c58322a469c7cbf5cabbdcb04a289aa118
Dmitri Astafiev <demius.md@gmail.com>
2020-09-17 16:05:14 +0300

    add empty strings for clarity

--- src/environment.ts
+++ src/environment.ts
@@ -2,8 +2,8 @@ console.info(`frontend-ws in ${process.env.NODE_ENV} mode`);
 
 export const ENVIRONMENT = process.env.NODE_ENV === 'development' ? {
     productionMode: false,
-    authServiceUrl: 'https://localhost:8443/api',
-    billingServiceUrl: 'https://localhost:8443/api/work/billing',
+    authServiceUrl: 'https://localhost:8443',
+    billingServiceUrl: 'https://localhost:8443/work/billing',
     baseUrl: ""
   } : {
     productionMode: true,
